d6bfc13349170e072a386106bf6908c876317bf3
GoldenGAAAR <p24x8A55Ozx1>
2022-05-15 18:55:16 +0300

    test4

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,17 @@
-#include <example.hpp>
+#include <hash.hpp>
 
-int main() {
-  example();
+int main(int argc, char* argv[]) {
+  if (argc == 1) {
+    std::cout << "NO JSON FILE PATH" << std::endl;
+  } else if (argc == 2) {
+    Hash hash(argv[1]);
+    hash.start();
+  } else if (argc == 3) {
+    int temp = (*argv[2] - '0');
+    Hash hash(argv[1], temp);
+    hash.start();
+  } else {
+    std::cout << "TOO MUCH ARGUMENTS" << std::endl;
+  }
+  return 0;
 }
